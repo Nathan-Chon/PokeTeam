a2e4a7dc422726079f9b7e68792e64dc62508f70
Nathan Chon <nchon99@gmail.com>
2023-03-01 22:33:42 +0000

    Search function finished

--- js/data.js
+++ js/data.js
@@ -1,1 +1,15 @@
 /* exported data */
+var data = {
+  entries: [],
+  view: ''
+};
+
+window.addEventListener('beforeunload', function (event) {
+  var formJSON = JSON.stringify(data);
+  localStorage.setItem('javascript-local-storage', formJSON);
+});
+
+var dataInJSON = localStorage.getItem('javascript-local-storage');
+if (dataInJSON !== null) {
+  data = JSON.parse(dataInJSON);
+}
---
+++ js/main.js
@@ -0,0 +1,15 @@
+var search = document.querySelector('.search-icon');
+var searchInput = document.querySelector('.search');
+
+search.addEventListener('click', function (event) {
+  event.preventDefault();
+  var input = searchInput.value;
+  var xhr = new XMLHttpRequest();
+  xhr.open('GET', 'https://pokeapi.co/api/v2/pokemon/' + input);
+  xhr.responseType = 'json';
+  xhr.addEventListener('load', function () {
+    console.log('xhr.status', xhr.status);
+    console.log('xhr.repsonse', xhr.response);
+  });
+  xhr.send();
+});
