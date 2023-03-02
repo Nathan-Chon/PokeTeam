0f885ad47a33cba060ced16b61a05e76f0207bd3
Nathan Chon <nchon99@gmail.com>
2023-03-02 08:07:56 +0000

    HTML and CSS Changes made and fucntion added

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 var search = document.querySelector('.search-icon');
 var searchInput = document.querySelector('.search');
+var $imageName = document.querySelector('.img-name');
 
 search.addEventListener('click', function (event) {
   event.preventDefault();
@@ -8,6 +9,23 @@ search.addEventListener('click', function (event) {
   xhr.open('GET', 'https://pokeapi.co/api/v2/pokemon/' + input);
   xhr.responseType = 'json';
   xhr.addEventListener('load', function () {
+    if (xhr.status === 200) {
+      var $imageTitle = document.createElement('h2');
+      $imageTitle.setAttribute('class', 'image-title');
+      $imageTitle.textContent = input;
+      $imageName.appendChild($imageTitle);
+      var $addButton = document.createElement('i');
+      $addButton.setAttribute('class', 'fa-solid fa-plus fa-2x add-image-icon');
+      $imageName.appendChild($addButton);
+      var $image = document.createElement('img');
+      $image.setAttribute('class', 'search-img');
+      $image.setAttribute('src', xhr.response.sprites.front_default);
+      $imageName.appendChild($image);
+      var $addButtonListener = document.querySelector('.add-image-icon');
+      $addButtonListener.addEventListener('click', function (event) {
+        data.entries.push(xhr.response);
+      });
+    }
     // console.log('xhr.status', xhr.status);
     // console.log('xhr.repsonse', xhr.response);
   });
