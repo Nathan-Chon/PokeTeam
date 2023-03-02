2ceca03d88c7410927daaf22f91c65d10af35a5b
Nathan Chon <nchon99@gmail.com>
2023-03-02 22:49:58 +0000

    Revised issue #2

--- js/main.js
+++ js/main.js
@@ -2,35 +2,21 @@ var search = document.querySelector('.search-icon');
 var searchInput = document.querySelector('.search');
 var $imageName = document.querySelector('.img-name');
 var $searchAddMember = document.querySelector('.search-add-member');
+var dataResponse;
 
 search.addEventListener('click', function (event) {
   event.preventDefault();
-  var input = searchInput.value;
   var xhr = new XMLHttpRequest();
+  var input = searchInput.value;
   xhr.open('GET', 'https://pokeapi.co/api/v2/pokemon/' + input);
   xhr.responseType = 'json';
+
   xhr.addEventListener('load', function () {
+    dataResponse = xhr.response;
     if (xhr.status === 200) {
       $searchAddMember.classList.remove('hidden');
-      var $imageTitle = document.createElement('h2');
-      $imageTitle.setAttribute('class', 'image-title');
-      $imageTitle.textContent = input;
-      $imageName.appendChild($imageTitle);
-      var $addButton = document.createElement('i');
-      $addButton.setAttribute('class', 'fa-solid fa-plus fa-2x add-image-icon');
-      $imageName.appendChild($addButton);
-      var $image = document.createElement('img');
-      $image.setAttribute('class', 'search-img');
-      $image.setAttribute('src', xhr.response.sprites.front_default);
-      $imageName.appendChild($image);
-      var $addButtonListener = document.querySelector('.add-image-icon');
-      $addButtonListener.addEventListener('click', function (event) {
-        data.entries.push(xhr.response);
-        $searchAddMember.classList.add('hidden');
-        $imageTitle.remove();
-        $addButton.remove();
-        $image.remove();
-      });
+      pokemonAdd(xhr.response);
+      searchInput.value = '';
     }
     // console.log('xhr.status', xhr.status);
     // console.log('xhr.repsonse', xhr.response);
@@ -51,4 +37,29 @@ searchButton.addEventListener('click', function (event) {
 partyButton.addEventListener('click', function (event) {
   searchTab.classList.add('hidden');
   partyTab.classList.remove('hidden');
+});
+
+function pokemonAdd(event) {
+  var $imageTitle = document.createElement('h2');
+  $imageTitle.setAttribute('class', 'image-title');
+  $imageTitle.textContent = searchInput.value;
+  $imageName.appendChild($imageTitle);
+  var $addButton = document.createElement('i');
+  $addButton.setAttribute('class', 'fa-solid fa-plus fa-2x add-image-icon');
+  $imageName.appendChild($addButton);
+  var $image = document.createElement('img');
+  $image.setAttribute('class', 'search-img');
+  $image.setAttribute('src', event.sprites.front_default);
+  $imageName.appendChild($image);
+}
+
+$imageName.addEventListener('click', function (event) {
+  if (event.target.tagName === 'I') {
+    var $div = event.target.closest('div');
+    $div.firstChild.nextSibling.remove();
+    $div.firstChild.nextSibling.remove();
+    $div.firstChild.nextSibling.remove();
+    $searchAddMember.classList.add('hidden');
+    data.entries.push(dataResponse);
+  }
 });
