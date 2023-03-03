58f666feec511fa5d7ff93f2f8fb8d69ba1ba536
Nathan Chon <nchon99@gmail.com>
2023-03-03 09:02:40 +0000

    inprogress of adding data to party

--- js/main.js
+++ js/main.js
@@ -3,6 +3,7 @@ var searchInput = document.querySelector('.search');
 var $imageName = document.querySelector('.img-name');
 var $searchAddMember = document.querySelector('.search-add-member');
 var dataResponse;
+var $partyMembers = document.querySelector('.party-members');
 
 search.addEventListener('click', function (event) {
   event.preventDefault();
@@ -61,5 +62,95 @@ $imageName.addEventListener('click', function (event) {
     $div.firstChild.nextSibling.remove();
     $searchAddMember.classList.add('hidden');
     data.entries.push(dataResponse);
+    renderPokemon(data.entries[4]);
   }
 });
+
+function renderPokemon(entry) {
+
+  var $row = document.createElement('div');
+  $row.setAttribute('class', 'row');
+  $partyMembers.appendChild($row);
+
+  var $columnHalf = document.createElement('div');
+  $columnHalf.setAttribute('class', 'column-half');
+  $row.appendChild($columnHalf);
+
+  var $pokemonGroup = document.createElement('div');
+  $pokemonGroup.setAttribute('class', 'pokemon-group');
+  $columnHalf.appendChild($pokemonGroup);
+  var $newTitle = document.createElement('h2');
+  $newTitle.setAttribute('class', 'new-title');
+  $pokemonGroup.appendChild($newTitle);
+  var $newImage = document.createElement('img');
+  $newImage.setAttribute('class', 'new-image');
+  $newImage.setAttribute('src', entry.sprites.front_default);
+  $pokemonGroup.appendChild($newImage);
+
+  var $abilitiesTitle = document.createElement('p');
+  $abilitiesTitle.setAttribute('class', 'ability-title');
+  $abilitiesTitle.textContent = 'Abilities:';
+  $pokemonGroup.appendChild($abilitiesTitle);
+  var $abilities = document.createElement('p');
+  for (var j = 0; j < entry.abilities.length; j++) {
+    $abilities.textContent += entry.abilities[j].ability.name + ' ';
+  }
+  $abilities.setAttribute('class', 'abilities');
+  $pokemonGroup.appendChild($abilities);
+
+  var $typeTitle = document.createElement('p');
+  $typeTitle.setAttribute('class', 'type-title');
+  $typeTitle.textContent = 'Type:';
+  $pokemonGroup.appendChild($typeTitle);
+  var $type = document.createElement('p');
+  for (var k = 0; k < entry.types.length; k++) {
+    $type.textContent += entry.types[k].type.name + ' ';
+  }
+  $type.setAttribute('class', 'types');
+  $pokemonGroup.appendChild($type);
+
+  var $levelTitle = document.createElement('p');
+  $levelTitle.setAttribute('class', 'level-title');
+  $levelTitle.textContent = 'Level:';
+  $pokemonGroup.appendChild($levelTitle);
+  var $moveTitle = document.createElement('p');
+  $moveTitle.setAttribute('class', 'move-title');
+  $moveTitle.textContent = 'Moves:';
+  $pokemonGroup.appendChild($moveTitle);
+
+  var $levels = document.createElement('p');
+  $levels.setAttribute('class', 'level-numbers');
+  var array = [];
+  for (var i = 0; i < entry.moves.length; i++) {
+    if (entry.moves[i].version_group_details[0].level_learned_at > 0) {
+      array.push(parseInt(entry.moves[i].version_group_details[0].level_learned_at));
+    }
+  }
+  array.sort(function (a, b) {
+    if (a > b) return 1;
+    if (a < b) return -1;
+    return 0;
+  });
+  var string = array.join(' \n ');
+  $levels.textContent = string;
+  $pokemonGroup.appendChild($levels);
+
+  var $moves = document.createElement('p');
+  $moves.setAttribute('class', 'moves');
+  var array1 = [];
+  for (var l = 0; l < entry.moves.length; l++) {
+    if (entry.moves[l].version_group_details[0].level_learned_at > 0) {
+      array1.push(entry.moves[l].move.name);
+    }
+  }
+  array1.sort(function (a, b) {
+    if (a > b) return 1;
+    if (a < b) return -1;
+    return 0;
+  });
+  var string1 = array1.join(' \r\n ');
+  $moves.textContent = string1;
+  $pokemonGroup.appendChild($moves);
+}
+
+// console.log(renderPokemon(data.entries[4]));
